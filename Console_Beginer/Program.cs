1801651b751c9df9f4bd02a08ffe2fd2566c0563
Dollyhoang <55663762+Dollyhoang@users.noreply.github.com>
2020-06-15 14:07:05 +0700

    Revert "update 1:56 15/6/2020"
    This reverts commit 63ea403564c573975ccc3f3d9ca6855c236e8aa9.

--- Console_Beginer/Program.cs
+++ Console_Beginer/Program.cs
@@ -19,7 +19,6 @@ namespace Console_Beginer
             Console.WriteLine("kêt quả đầu tiên:"+value);
             Increate(ref value);
             Console.WriteLine("Kết quả sau khi tăng với ref:" +value);
-            Console.WriteLine("Kết quả mới là:Test thôi ");
             Console.ReadKey();
         }
     }
